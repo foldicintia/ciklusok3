802b28d9820c5a0c82f3ef255662f53c8a999702
foldicintia <foldicintia2002@gmail.com>
2023-10-19 12:20:03 +0200

    ciklusok3_gyakorlas

---
+++ main.py
@@ -0,0 +1,17 @@
+import ciklusok3
+import math
+
+
+"""
+szam: int = int(input("Adj meg egy számot!"))
+ciklusok3.feladat1(szam)
+
+
+a: int = int(input("Adj meg egy pozitív egész számot!"))
+ciklusok3.feladat2(a)
+
+a: int = int(input("Adj meg egy számot!"))
+b: int = int(input("Adj meg még egy számot!"))
+ciklusok3.feladat3(a,b)
+"""
+ciklusok3.feladat4()
